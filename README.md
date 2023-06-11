0ec30807ede405f84b521bd52f05e4f0652178d3
Andrey Enin <enotramone@gmail.com>
2023-06-11 11:08:01 +0200

    Translate comments

--- README.md
+++ README.md
@@ -1,16 +1,41 @@
+# Where is ISS
+
+Serverless [skill for Alice](https://alice.ya.ru/s/38592329-ff16-41ac-b0b5-605669e888e7) ([virtual assistant](https://en.wikipedia.org/wiki/Alice_(virtual_assistant))) based on Node.js.
+
+It informs which area of the earth the international space station is currently flying over.
+
+### How does it work?
+
+The user says: «*Alice, launch the skill Where is the ISS*».
+
+Alice responds with a welcome skill message: «*The ISS is in orbit. To clarify where it is flying now?*»
+
+On any following phrase of the user occurs:
+* Going to the [Open Notify API](http://open-notify.org/Open-Notify-API/ISS-Location-Now/) for the current coordinates of the ISS;
+* Transmitting coordinates to the [Geocoder](https://yandex.com/dev/maps/geocoder/) with the receipt of a locality in response;
+* In the case of overflight over an unpopulated area, then the definitions of the hemisphere;
+* Response to the user about the location of the station (locality or hemisphere).
+
+The deployment occurs via the [Yandex Cloud Functions](https://cloud.yandex.com/en/services/functions).
+
+----
+
 # Где МКС
 
-Serverless [навык Алисы](https://alice.ya.ru/s/38592329-ff16-41ac-b0b5-605669e888e7) на Node.js. Сообщает над какой областью пролетает международная космическая станция.
+Бессерверный [навык Алисы](https://alice.ya.ru/s/38592329-ff16-41ac-b0b5-605669e888e7) на Node.js.
 
-### Как это работает?
+Сообщает над какой областью земли сейчас пролетает международная космическая станция.
 
-Пользователь говорит: «*Алиса, запусти навык где мкс*»;
+### Как он работает?
 
-Алиса отвечает приветственным сообщением навыка: «*мкс на орбите. уточнить где сейчас пролетает*»;
+Пользователь говорит: «*Алиса, запусти навык где мкс*».
 
-На любую следующую фразу происходит:
-* Поход в [Open Notify API](http://open-notify.org/Open-Notify-API/ISS-Location-Now/) за текущеми координатами МКС;
+Алиса отвечает приветственным сообщением навыка: «*мкс на орбите. уточнить где сейчас пролетает*».
+
+На любую следующую фразу пользователя происходит:
+* Поход в [Open Notify API](http://open-notify.org/Open-Notify-API/ISS-Location-Now/) за текущими координатами МКС;
 * Передача координат в [Геокодер](https://tech.yandex.ru/maps/geocoder/) с получением в ответ населенного пункта;
-* В случае пролета над безлюдной территорией, то определения полушария.
+* В случае пролета над безлюдной территорией, то определения полушария;
+* Ответ пользователю о местонахождении станции (населенный пункт или полушарие).
 
 Деплой происходит через [Функцию в Яндекс.Облаке](https://yandex.ru/dev/dialogs/alice/doc/deploy-ycloud-function-docpage/).
--- index.js
+++ index.js
@@ -6,25 +6,29 @@ const http = require("http");
 // https://nodejs.org/api/https.html
 const https = require("https");
 
-// API трекинга МКС http://open-notify.org/Open-Notify-API/ISS-Location-Now/
+// International Space Station Current Location
 // http://open-notify.org/Open-Notify-API/ISS-Location-Now/
 const apiIss = "http://api.open-notify.org/iss-now.json";
-// API Яндекс.Карт
-// https://tech.yandex.ru/maps/geocoder/doc/desc/concepts/input_params-docpage/
+// Yandex.Maps API / API Яндекс.Карт
+// https://yandex.ru/dev/geocode/doc/ru/concepts/input-params
+// https://yandex.ru/dev/geocode/doc/en/concepts/input-params
 const apiGeo = "https://geocode-maps.yandex.ru/1.x/";
+// Secret key is stored in environment variable
 // Секретик ключа лежит в переменной окружения
 // https://nodejs.org/api/process.html#process_process_env
 const apiGeoKey = process.env.key;
 
+// For a serverless application, you need to write API calls on a «pure node» (probably)
 // Для serverless приложения нужно написать вызовы к API на «чистой ноде» (наверное)
-// https://cloud.yandex.ru/docs/functions/concepts/function
-// С решением вопроса записи ответа в переменную помог https://github.com/k03mad
-// «обернул в промис, чтобы можно было последовательно вызывать»
+// https://cloud.yandex.com/en/docs/functions/concepts/function
+// To write the response to a variable, helped https://github.com/k03mad, you need to
+// «wrap it in a promise so that you can consistently call it»
+// Для записи ответа в переменную помог https://github.com/k03mad, нужно
+// «обернуть в промис, чтобы можно было последовательно вызывать»
 const getHttp = url => new Promise((resolve, reject) => {
     http.get(url, res => {
-        // В ноде нет возможности сразу получить тело запроса
+        // There is no way to get the request body immediately in the «pure node»
         // https://stackoverflow.com/questions/6968448/where-is-body-in-a-nodejs-http-get-response
-        // Нужно использовать пример из документации
         if (res.statusCode === 200) {
             let rawData = "";
             res.on("data", chunk => {
@@ -69,10 +73,13 @@ const getHttps = url => new Promise((resolve, reject) => {
     });
 });
 
+// Creating skills for Alice
 // Создание навыка в Яндекс.Облаке
-// https://cloud.yandex.ru/docs/functions/solutions/alice-skill
+// https://cloud.yandex.com/en/docs/functions/tutorials/alice-skill
+// Creating a serverless function in Yandex.Cloud
 // Создание функции в Яндекс.Облаке
 // https://yandex.ru/dev/dialogs/alice/doc/deploy-ycloud-function-docpage/#create-function
+// Example of a skill using a serverless function
 // Пример навыка с использованием serverless функции
 // https://github.com/yandex-cloud/examples/blob/master/serverless/functions/alice/nodejs/parrot/index.js
 /**
@@ -86,12 +93,16 @@ module.exports.handler = async (event) => {
     // https://yandex.ru/dev/dialogs/alice/doc/protocol-docpage/
     const {request, version, session} = event;
 
+    // Skill MUST contain welcome message and help
     // Навык ДОЛЖЕН содержать приветственное сообщение и help
     // https://yandex.ru/dev/dialogs/alice/doc/requirements-docpage/#specific__content
+    // "The ISS is in orbit. To clarify where it is flying now?"
     const textWelcome = "МКС на орбите. Уточнить где сейчас пролетает?";
+    // "To any phrase I will answer what the ISS is flying over"
     const textHelp = "На любую фразу отвечу над чем пролетает МКС";
 
-    // Ответы подсмотрены в навыке
+    // The ways of answers are spied in the skill:
+    // Способы ответов подсмотрены в навыке:
     // https://github.com/tadatuta/alice-songs/blob/master/lib/logic.js
     if (session.new) {
         return {
@@ -106,6 +117,7 @@ module.exports.handler = async (event) => {
 
     const command = request.command.toLowerCase();
 
+    // "help" || "what can you do"
     if (command.includes("помощь") || command.includes("что ты умеешь")) {
         return {
             version,
@@ -118,9 +130,9 @@ module.exports.handler = async (event) => {
     }
 
     const resIss = await getHttp(apiIss);
-    // Долгота
+    // Longitude / Долгота
     let long = Number(resIss.iss_position.longitude);
-    // Широта
+    // Latitude / Широта
     let lat = Number(resIss.iss_position.latitude);
 
     // «Request path should include query string»
@@ -141,10 +153,13 @@ module.exports.handler = async (event) => {
     let textGeo;
 
     if (foundObjects === 0 && lat > 0) {
+        // "Somewhere over the northern hemisphere"
         textGeo = "Где-то над северным полушарием";
     } else if (foundObjects === 0 && lat < 0) {
+        // "Somewhere over the southern hemisphere"
         textGeo = "Где-то над южным полушарием";
     } else if (foundObjects === 0 && lat === 0) {
+        // "Somewhere above the equator"
         textGeo = "Где-то над экватором";
     } else {
         textGeo = resGeo.response.GeoObjectCollection.featureMember[0].GeoObject.metaDataProperty.GeocoderMetaData.text;
@@ -155,6 +170,7 @@ module.exports.handler = async (event) => {
         session,
         response: {
             text: textGeo,
+            // Finish dialog after answer
             // Завершаем разговор после ответа
             end_session: true
         }
