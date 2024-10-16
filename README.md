f3bbcb049882074d09d679e68e6509ee4458fc76
Andrey Enin <enotramone@gmail.com>
2024-10-16 20:00:58 +0200

    Switch to MapBox API

--- README.md
+++ README.md
@@ -1,41 +1,20 @@
-# Where is ISS
+# Where is the ISS
 
-Serverless [skill for Alice](https://dialogs.yandex.ru/store/skills/3b4d3762-gde-mks) ([virtual assistant](https://en.wikipedia.org/wiki/Alice_(virtual_assistant))) based on Node.js.
+Serverless [skill for Alice](https://dialogs.yandex.com/store/skills/3b4d3762-gde-mks) ([virtual assistant](<https://en.wikipedia.org/wiki/Alice_(virtual_assistant)>)) based on Node.js.
 
-It informs which area of the earth the international space station is currently flying over.
+It informs which area of the earth the International Space Station is currently flying over.
 
 ### How does it work?
 
-The user says: «*Alice, launch the skill Where is the ISS*».
+The user says: «_Alice, launch the skill Where is the ISS_».
 
-Alice responds with a welcome skill message: «*The ISS is in orbit. To clarify where it is flying now?*»
+Alice responds with a welcome skill message: «_The ISS is in orbit. To clarify where it is flying now?_»
 
 On any following phrase of the user occurs:
-* Going to the [Open Notify API](http://open-notify.org/Open-Notify-API/ISS-Location-Now/) for the current coordinates of the ISS;
-* Transmitting coordinates to the [Geocoder](https://yandex.com/dev/maps/geocoder/) with the receipt of a locality in response;
-* In the case of overflight over an unpopulated area, then the definitions of the hemisphere;
-* Response to the user about the location of the station (locality or hemisphere).
+
+1. Going to the [Open Notify API](http://open-notify.org/Open-Notify-API/ISS-Location-Now/) for the current coordinates of the ISS;
+2. Transmitting coordinates to the [MapBox Reverse geocoding API](https://docs.mapbox.com/api/search/geocoding/#reverse-geocoding) with the response of a current locality;
+3. In the case of overflight over an unpopulated area, then the definitions of the hemisphere;
+4. Response to the user about the location of the station (locality or hemisphere).
 
 The deployment occurs via the [Yandex Cloud Functions](https://cloud.yandex.com/en/services/functions).
-
-----
-
-# Где МКС
-
-Бессерверный [навык Алисы](https://dialogs.yandex.ru/store/skills/3b4d3762-gde-mks) на Node.js.
-
-Сообщает над какой областью земли сейчас пролетает международная космическая станция.
-
-### Как он работает?
-
-Пользователь говорит: «*Алиса, запусти навык где мкс*».
-
-Алиса отвечает приветственным сообщением навыка: «*мкс на орбите. уточнить где сейчас пролетает*».
-
-На любую следующую фразу пользователя происходит:
-* Поход в [Open Notify API](http://open-notify.org/Open-Notify-API/ISS-Location-Now/) за текущими координатами МКС;
-* Передача координат в [Геокодер](https://tech.yandex.ru/maps/geocoder/) с получением в ответ населенного пункта;
-* В случае пролета над безлюдной территорией, то определения полушария;
-* Ответ пользователю о местонахождении станции (населенный пункт или полушарие).
-
-Деплой происходит через [Функцию в Яндекс.Облаке](https://yandex.ru/dev/dialogs/alice/doc/deploy-ycloud-function-docpage/).
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 // https://nodejs.org/api/querystring.html
-const {stringify} = require("querystring");
+const { stringify } = require("querystring");
 // https://nodejs.org/api/http.html
 const http = require("http");
 // https://nodejs.org/api/https.html
@@ -9,78 +9,76 @@ const https = require("https");
 // International Space Station Current Location
 // http://open-notify.org/Open-Notify-API/ISS-Location-Now/
 const apiIss = "http://api.open-notify.org/iss-now.json";
-// Yandex.Maps API / API Яндекс.Карт
-// https://yandex.ru/dev/geocode/doc/ru/concepts/input-params
-// https://yandex.ru/dev/geocode/doc/en/concepts/input-params
-const apiGeo = "https://geocode-maps.yandex.ru/1.x/";
+// MapBox Geocoding API
+// https://docs.mapbox.com/api/search/geocoding/
+const apiGeocode = "https://api.mapbox.com/search/geocode/v6/reverse";
 // Secret key is stored in environment variable
-// Секретик ключа лежит в переменной окружения
 // https://nodejs.org/api/process.html#process_process_env
-const apiGeoKey = process.env.key;
+const apiGeocodeKey = process.env.key;
 
 // For a serverless application, you need to write API calls on a «pure node» (probably)
-// Для serverless приложения нужно написать вызовы к API на «чистой ноде» (наверное)
 // https://cloud.yandex.com/en/docs/functions/concepts/function
 // To write the response to a variable, helped https://github.com/k03mad, you need to
 // «wrap it in a promise so that you can consistently call it»
-// Для записи ответа в переменную помог https://github.com/k03mad, нужно
-// «обернуть в промис, чтобы можно было последовательно вызывать»
-const getHttp = url => new Promise((resolve, reject) => {
-    http.get(url, res => {
-        // There is no way to get the request body immediately in the «pure node»
-        // https://stackoverflow.com/questions/6968448/where-is-body-in-a-nodejs-http-get-response
-        if (res.statusCode === 200) {
-            let rawData = "";
-            res.on("data", chunk => {
-                rawData += chunk;
-            });
-            res.on("end", () => {
-                try {
-                    const response = JSON.parse(rawData);
-                    resolve(response);
-                } catch (err) {
-                    reject(err.message);
-                }
-            }).on("error", err => {
-                console.log(`Error: ${err.message}`);
-            });
-        } else {
-            console.log("Error");
-        }
+const getHttp = (url) =>
+  new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+      // There is no way to get the request body immediately in the «pure node»
+      // https://stackoverflow.com/questions/6968448/where-is-body-in-a-nodejs-http-get-response
+      if (res.statusCode === 200) {
+        let rawData = "";
+        res.on("data", (chunk) => {
+          rawData += chunk;
+        });
+        res
+          .on("end", () => {
+            try {
+              const response = JSON.parse(rawData);
+              resolve(response);
+            } catch (err) {
+              reject(err.message);
+            }
+          })
+          .on("error", (err) => {
+            console.log(`Error: ${err.message}`);
+          });
+      } else {
+        console.log("Error");
+      }
     });
-});
+  });
 
-const getHttps = url => new Promise((resolve, reject) => {
-    https.get(url, res => {
-        if (res.statusCode === 200) {
-            let rawData = "";
-            res.on("data", chunk => {
-                rawData += chunk;
-            });
-            res.on("end", () => {
-                try {
-                    const response = JSON.parse(rawData);
-                    resolve(response);
-                } catch (err) {
-                    reject(err.message);
-                }
-            }).on("error", err => {
-                console.log(`Error: ${err.message}`);
-            });
-        } else {
-            console.log("Error");
-        }
+const getHttps = (url) =>
+  new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      if (res.statusCode === 200) {
+        let rawData = "";
+        res.on("data", (chunk) => {
+          rawData += chunk;
+        });
+        res
+          .on("end", () => {
+            try {
+              const response = JSON.parse(rawData);
+              resolve(response);
+            } catch (err) {
+              reject(err.message);
+            }
+          })
+          .on("error", (err) => {
+            console.log(`Error: ${err.message}`);
+          });
+      } else {
+        console.log("Error");
+      }
     });
-});
+  });
 
 // Creating skills for Alice
-// Создание навыка в Яндекс.Облаке
 // https://cloud.yandex.com/en/docs/functions/tutorials/alice-skill
 // Creating a serverless function in Yandex.Cloud
-// Создание функции в Яндекс.Облаке
 // https://yandex.ru/dev/dialogs/alice/doc/deploy-ycloud-function-docpage/#create-function
 // Example of a skill using a serverless function
-// Пример навыка с использованием serverless функции
 // https://github.com/yandex-cloud/examples/blob/master/serverless/functions/alice/nodejs/parrot/index.js
 /**
  * Entry-point for Serverless Function.
@@ -90,89 +88,86 @@ const getHttps = url => new Promise((resolve, reject) => {
  * @return {Promise<Object>} response to be serialized as JSON.
  */
 module.exports.handler = async (event) => {
-    // https://yandex.ru/dev/dialogs/alice/doc/protocol-docpage/
-    const {request, version, session} = event;
+  // https://yandex.ru/dev/dialogs/alice/doc/protocol-docpage/
+  const { request, version, session } = event;
 
-    // Skill MUST contain welcome message and help
-    // Навык ДОЛЖЕН содержать приветственное сообщение и help
-    // https://yandex.ru/dev/dialogs/alice/doc/requirements-docpage/#specific__content
-    // "The ISS is in orbit. To clarify where it is flying now?"
-    const textWelcome = "МКС на орбите. Уточнить где сейчас пролетает?";
-    // "To any phrase I will answer what the ISS is flying over"
-    const textHelp = "На любую фразу отвечу над чем пролетает МКС";
+  // Skill MUST contain welcome message and help
+  // https://yandex.ru/dev/dialogs/alice/doc/requirements-docpage/#specific__content
+  // "The ISS is in orbit. To clarify where it is flying now?"
+  const textWelcome = "МКС на орбите. Уточнить где сейчас пролетает?";
+  // "To any phrase I will answer what the ISS is flying over"
+  const textHelp = "На любую фразу отвечу над чем пролетает МКС";
 
-    // The ways of answers are spied in the skill:
-    // Способы ответов подсмотрены в навыке:
-    // https://github.com/tadatuta/alice-songs/blob/master/lib/logic.js
-    if (session.new) {
-        return {
-            version,
-            session,
-            response: {
-                text: textWelcome,
-                end_session: false
-            }
-        };
-    }
-
-    const command = request.command.toLowerCase();
-
-    // "help" || "what can you do"
-    if (command.includes("помощь") || command.includes("что ты умеешь")) {
-        return {
-            version,
-            session,
-            response: {
-                text: textHelp,
-                end_session: false
-            }
-        };
-    }
-
-    const resIss = await getHttp(apiIss);
-    // Longitude / Долгота
-    let long = Number(resIss.iss_position.longitude);
-    // Latitude / Широта
-    let lat = Number(resIss.iss_position.latitude);
-
-    // «Request path should include query string»
-    const queryData = stringify({
-        geocode: `${long},${lat}`,
-        apikey: apiGeoKey,
-        sco: "longlat",
-        kind: "locality",
-        format: "json",
-        results: 1,
-        lang: "ru_RU"
-    });
-
-    const resGeo = await getHttps(`${apiGeo}?${queryData}`);
-
-    let foundObjects = resGeo.response.GeoObjectCollection.featureMember.length;
-
-    let textGeo;
-
-    if (foundObjects === 0 && lat > 0) {
-        // "Somewhere over the northern hemisphere"
-        textGeo = "Где-то над северным полушарием";
-    } else if (foundObjects === 0 && lat < 0) {
-        // "Somewhere over the southern hemisphere"
-        textGeo = "Где-то над южным полушарием";
-    } else if (foundObjects === 0 && lat === 0) {
-        // "Somewhere above the equator"
-        textGeo = "Где-то над экватором";
-    } else {
-        textGeo = resGeo.response.GeoObjectCollection.featureMember[0].GeoObject.metaDataProperty.GeocoderMetaData.text;
-    }
-
+  // The ways of answers are spied in the skill:
+  // https://github.com/tadatuta/alice-songs/blob/master/lib/logic.js
+  if (session.new) {
     return {
-        version,
-        session,
-        response: {
-            text: textGeo,
-            // Finish dialog after answer
-            // Завершаем разговор после ответа
-            end_session: true
-        }
+      version,
+      session,
+      response: {
+        text: textWelcome,
+        end_session: false,
+      },
     };
+  }
+
+  const command = request.command.toLowerCase();
+
+  // "help" || "what can you do"
+  if (command.includes("помощь") || command.includes("что ты умеешь")) {
+    return {
+      version,
+      session,
+      response: {
+        text: textHelp,
+        end_session: false,
+      },
+    };
+  }
+
+  const resIss = await getHttp(apiIss);
+  // Longitude / Долгота
+  let long = Number(resIss.iss_position.longitude);
+  // Latitude / Широта
+  let lat = Number(resIss.iss_position.latitude);
+
+  // Request path should include query string
+  // https://docs.mapbox.com/api/search/geocoding/#reverse-geocoding
+  const queryData = stringify({
+    longitude: long,
+    latitude: lat,
+    access_token: `${apiGeocodeKey}`,
+    types: "place",
+    language: "ru",
+    worldview: "ru",
+  });
+
+  const resGeocode = await getHttps(`${apiGeocode}?${queryData}`);
+
+  let foundObjects = resGeocode.features;
+
+  let textGeo;
+
+  if (foundObjects.length === 0 && lat === 0) {
+    // "Somewhere above the equator"
+    textGeo = "Где-то над экватором";
+  } else if (foundObjects.length === 0 && lat > 0) {
+    // "Somewhere over the northern hemisphere"
+    textGeo = "Где-то над северным полушарием";
+  } else if (foundObjects.length === 0 && lat < 0) {
+    // "Somewhere over the southern hemisphere"
+    textGeo = "Где-то над южным полушарием";
+  } else {
+    textGeo = foundObjects[0].properties.name_preferred;
+  }
+
+  return {
+    version,
+    session,
+    response: {
+      text: textGeo,
+      // Finish dialog after answer
+      end_session: true,
+    },
+  };
 };
